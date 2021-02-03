b3aebe8d3c4294b38f3c37fe7ea68fb4c21a783f
wcook499 <w.cook499@gmail.com>
2021-02-02 21:07:10 -0500

    updates

--- Payroll Phase 3B - Input From File/InputFromFile.cpp
+++ Payroll Phase 3B - Input From File/InputFromFile.cpp
@@ -1,26 +1,32 @@
+/*
+WILLIAM COOK
+2/14/21
+
+This program opens a file and reads employee information, then outputs the same
+information as the Phase 2B Net Pay program.
+*/
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main(){
-    int employeeID;
+    int employeeID, counter, inputQuantity;
     float hoursWorked, hourlyPay, grossPay, taxRate, taxAmount, netPay;
     taxRate = 0.10;
-    cout << "ENTER EMPLOYEE ID: ";
-    cin >> employeeID;
-    cout << "ENTER HOURS WORKED: ";
-    cin >> hoursWorked;
-    cout << "ENTER HOURLY PAY: $";
-    cin >> hourlyPay;
-    grossPay = hoursWorked * hourlyPay;
-    taxAmount = grossPay * taxRate;
-    netPay = grossPay - taxAmount;
-    cout << "\n";
-    cout << "YOUR ID IS: " << employeeID << "\n";
-    cout << "YOUR HOURS WORKED IS: " << hoursWorked << "\n";
-    cout << "YOUR HOURLY RATE IS: $" << hourlyPay << "\n";
-    cout << "YOUR GROSS PAY IS: $" << grossPay << "\n";
-    cout << "YOUR TAX RATE IS: " << taxRate << "\n";
-    cout << "YOUR TAX AMOUNT IS: $" << taxAmount << "\n";
-    cout << "YOUR NET PAY IS: $" << netPay << "\n";
+
+    ifstream fin("./employee.in");
+    while(fin >> employeeID >> hoursWorked >> hourlyPay){
+        grossPay = hoursWorked * hourlyPay;
+        taxAmount = grossPay * taxRate;
+        netPay = grossPay - taxAmount;
+        cout << endl;
+        cout << "YOUR ID IS: " << employeeID << endl;
+        cout << "YOUR HOURS WORKED IS: " << hoursWorked << endl;
+        cout << "YOUR HOURLY RATE IS: $" << hourlyPay << endl;
+        cout << "YOUR GROSS PAY IS: $" << grossPay << endl;
+        cout << "YOUR TAX RATE IS: " << taxRate << endl;
+        cout << "YOUR TAX AMOUNT IS: $" << taxAmount << endl;
+        cout << "YOUR NET PAY IS: $" << netPay << endl;
+    }//WHILE
     return 0;
-}
+}//MAIN
