12e76701fe4a4913a1967e8cbcf8ddd60a38afc5
wcook499 <w.cook499@gmail.com>
2021-02-03 21:28:17 -0500

    Copy code from "GraduatedTaxRates.cpp" as starting point

--- Payroll Phase 4B - Overtime/Overtime.cpp
+++ Payroll Phase 4B - Overtime/Overtime.cpp
@@ -10,15 +10,32 @@ information as the Phase 2B Net Pay program.
 using namespace std;
 
 int main(){
+    char maritalStatus;
     int employeeID, counter, inputQuantity;
     float hoursWorked, hourlyPay, grossPay, taxRate, taxAmount, netPay;
-    taxRate = 0.10;
 
     ifstream fin("./employee.in");
-    while(fin >> employeeID >> hoursWorked >> hourlyPay){
+    while(fin >> employeeID >> hoursWorked >> hourlyPay  >> maritalStatus){
+
+        //Determine Gross Pay based on hours worked and pay rate
         grossPay = hoursWorked * hourlyPay;
+
+        //Get base Tax Rate based on gross pay
+        if (grossPay > 1000) taxRate = 0.3;
+        else if (grossPay > 800) taxRate = 0.2;
+        else if (grossPay > 500) taxRate = 0.1;
+        else taxRate = 0.0;
+
+        //Modify base tax rate based on marital status:
+        //Married (NC), Head of Household (-5%), Single (+5%)
+        if ((maritalStatus == 's') || (maritalStatus == 'S')) taxRate += 0.05;
+        if ((maritalStatus == 'h') || (maritalStatus == 'H')) taxRate -= 0.05;
+        
+        //Determine Net Pay
         taxAmount = grossPay * taxRate;
         netPay = grossPay - taxAmount;
+        
+        //Display information to user
         cout << endl;
         cout << "YOUR ID IS: " << employeeID << endl;
         cout << "YOUR HOURS WORKED IS: " << hoursWorked << endl;
