5f6405195b46a41a3cf629dd6a240535de0ffb7d
wcook499 <w.cook499@gmail.com>
2021-02-22 22:27:02 -0500

    convert most variables to arrays

--- Payroll Phase 5A - Arrays/Arrays.cpp
+++ Payroll Phase 5A - Arrays/Arrays.cpp
@@ -10,52 +10,55 @@ information as the Phase 2B Net Pay program.
 using namespace std;
 
 int main(){
-    char maritalStatus;
-    int employeeID[100];
-    float hoursWorked, hourlyPay, overtimeHours, overtimePay, grossPay,
-    taxRate, taxAmount, netPay;
+    char maritalStatus[100];
+    int employeeID[100], i = 0;
+    float hoursWorked[100], hourlyPay[100], overtimeHours[100], overtimePay[100], grossPay[100],
+    taxRate[100], taxAmount[100], netPay[100];
 
     ifstream fin("./employee.in");
-    while(fin >> employeeID >> hoursWorked >> hourlyPay  >> maritalStatus){
+    while(fin >> employeeID[i] >> hoursWorked[i] >> hourlyPay[i]  >> maritalStatus[i]){
 
         //Determine Gross Pay based on hours worked and pay rate, calculating
         //overtime if necessary.
-        if (hoursWorked > 40){
-            overtimeHours = hoursWorked - 40;
-            overtimePay = (hourlyPay * 0.5) * overtimeHours;
-            grossPay = (hoursWorked * hourlyPay) + overtimePay;
+        if (hoursWorked[i] > 40){
+            overtimeHours[i] = hoursWorked[i] - 40;
+            overtimePay[i] = (hourlyPay[i] * 0.5) * overtimeHours[i];
+            grossPay[i] = (hoursWorked[i] * hourlyPay[i]) + overtimePay[i];
         }
-        else if (hoursWorked <= 40){
-            grossPay = hoursWorked * hourlyPay;
+        else if (hoursWorked[i] <= 40){
+            grossPay[i] = hoursWorked[i] * hourlyPay[i];
         }
 
         //Get base Tax Rate based on gross pay
-        if (grossPay > 1000) taxRate = 0.3;
-        else if (grossPay > 800) taxRate = 0.2;
-        else if (grossPay > 500) taxRate = 0.1;
-        else taxRate = 0.0;
+        if (grossPay[i] > 1000) taxRate[i] = 0.3;
+        else if (grossPay[i] > 800) taxRate[i] = 0.2;
+        else if (grossPay[i] > 500) taxRate[i] = 0.1;
+        else taxRate[i] = 0.0;
 
         //Modify base tax rate based on marital status:
         //Married (NC), Head of Household (-5%), Single (+5%)
-        if ((maritalStatus == 's') || (maritalStatus == 'S')) taxRate += 0.05;
-        if ((maritalStatus == 'h') || (maritalStatus == 'H')) taxRate -= 0.05;
+        if ((maritalStatus[i] == 's') || (maritalStatus[i] == 'S')) taxRate[i] += 0.05;
+        if ((maritalStatus[i] == 'h') || (maritalStatus[i] == 'H')) taxRate[i] -= 0.05;
         
         //Catch negative tax rates and correct to 0
-        if (taxRate < 0) taxRate = 0;
+        if (taxRate[i] < 0) taxRate[i] = 0;
 
         //Determine Net Pay
-        taxAmount = grossPay * taxRate;
-        netPay = grossPay - taxAmount;
+        taxAmount[i] = grossPay[i] * taxRate[i];
+        netPay[i] = grossPay[i] - taxAmount[i];
         
+        i++;
+
         //Display information to user
         cout << endl;
-        cout << "YOUR ID IS: " << employeeID << endl;
-        cout << "YOUR HOURS WORKED IS: " << hoursWorked << endl;
-        cout << "YOUR HOURLY RATE IS: $" << hourlyPay << endl;
-        cout << "YOUR GROSS PAY IS: $" << grossPay << endl;
-        cout << "YOUR TAX RATE IS: " << taxRate << endl;
-        cout << "YOUR TAX AMOUNT IS: $" << taxAmount << endl;
-        cout << "YOUR NET PAY IS: $" << netPay << endl;
+        cout << "INDEX IS: " << i << endl;
+        cout << "YOUR ID IS: " << employeeID[i] << endl;
+        cout << "YOUR HOURS WORKED IS: " << hoursWorked[i] << endl;
+        cout << "YOUR HOURLY RATE IS: $" << hourlyPay[i] << endl;
+        cout << "YOUR GROSS PAY IS: $" << grossPay[i] << endl;
+        cout << "YOUR TAX RATE IS: " << taxRate[i] << endl;
+        cout << "YOUR TAX AMOUNT IS: $" << taxAmount[i] << endl;
+        cout << "YOUR NET PAY IS: $" << netPay[i] << endl;
     }//WHILE
     return 0;
 }//MAIN
