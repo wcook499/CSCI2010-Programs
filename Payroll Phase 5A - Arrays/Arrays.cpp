ea18ad029139cb5bd4e1bb1653b30ce816d13d09
wcook499 <w.cook499@gmail.com>
2021-02-25 22:50:35 -0500

    ...

--- Payroll Phase 5A - Arrays/Arrays.cpp
+++ Payroll Phase 5A - Arrays/Arrays.cpp
@@ -24,17 +24,66 @@ int main(){
     //Declare variables
     const int arrayLen = numLines;
     char maritalStatus[arrayLen];
+    string firstName[arrayLen], lastName[arrayLen];
     int employeeID[arrayLen], i = 0;
     float hoursWorked[arrayLen], hourlyPay[arrayLen], overtimeHours[arrayLen], overtimePay[arrayLen], grossPay[arrayLen],
-    taxRate[arrayLen], taxAmount[arrayLen], netPay[arrayLen];
+    taxRate[arrayLen], taxAmount[arrayLen], netPay[arrayLen], regularPay[arrayLen];
 
 
     //Read all data into arrays
     ifstream fin("./employee.in");
     for (i = 0; i < arrayLen; i++){
-        fin >> employeeID[i] >> hoursWorked[i] >> hourlyPay[i]  >> maritalStatus[i];
+        fin >> firstName[i] >> lastName[i] >> maritalStatus[i] >> employeeID[i] >> hoursWorked[i] >> hourlyPay[i];
     }//for
 
+
+    //Determine Gross Pay based on hours worked and pay rate, calculating
+    //overtime if necessary.
+    for (i = 0; i < arrayLen; i++){
+        if (hoursWorked[i] > 40){
+            overtimeHours[i] = hoursWorked[i] - 40;
+            overtimePay[i] = (hourlyPay[i] * 0.5) * overtimeHours[i];
+            regularPay[i] = hoursWorked[i] * hourlyPay[i];
+            grossPay[i] = regularPay[i] + overtimePay[i];
+        }//if
+        else if (hoursWorked[i] <= 40){
+            grossPay[i] = hoursWorked[i] * hourlyPay[i];
+        }//else if
+    }//for
+
+
+    //Get base Tax Rate based on gross pay
+    if (grossPay[i] > 1000) taxRate[i] = 0.3;
+    else if (grossPay[i] > 800) taxRate[i] = 0.2;
+    else if (grossPay[i] > 500) taxRate[i] = 0.1;
+    else taxRate[i] = 0.0;
+
+    //Modify base tax rate based on marital status:
+    //Married (NC), Head of Household (-5%), Single (+5%)
+    if ((maritalStatus[i] == 's') || (maritalStatus[i] == 'S')) taxRate[i] += 0.05;
+    if ((maritalStatus[i] == 'h') || (maritalStatus[i] == 'H')) taxRate[i] -= 0.05;
+
+    //Catch negative tax rates and correct to 0
+    if (taxRate[i] < 0) taxRate[i] = 0;
+
+
+    //Determine Net Pay
+    taxAmount[i] = grossPay[i] * taxRate[i];
+    netPay[i] = grossPay[i] - taxAmount[i];
+
+    //Display information to user
+    cout << "DR. EBRAHIMI'S PAYROLL INSTITUTE" << endl;
+    cout << endl;
+    cout << setw(15) << "FIRST NAME" << setw(15) << "LAST NAME" << setw(15) << "STAT" <<  setw(15) << "SSN"
+         << setw(15) << "HW" << setw(15) << "HR" << setw(15) << "OTH" << setw(15) << "REGP"
+         << setw(15) << "GROSS" << setw(15) << "TAX" << setw(15) << "NET" << endl;
+    cout << string(165, '=') << endl;
+    for (i = 0; i < arrayLen; i++){
+        cout << setw(15) << firstName[i] << setw(15) << lastName[i] << setw(15) << maritalStatus[i] << setw(15)
+             << employeeID[i] << setw(15) << hoursWorked[i] << setw(15) << hourlyPay[i] << setw(15)
+             << overtimeHours[i] << setw(15) << regularPay[i] << setw(15) << grossPay[i] << setw(15)
+             << taxAmount[i] << setw(15) << netPay[i] << endl;
+    }
 }//MAIN
 
 
@@ -52,39 +101,12 @@ int main(){
 
 
 /*
-        //Determine Gross Pay based on hours worked and pay rate, calculating
-        //overtime if necessary.
-        for (i = 0; i < arrayLen; i++){
-            if (hoursWorked[i] > 40){
-                overtimeHours[i] = hoursWorked[i] - 40;
-                overtimePay[i] = (hourlyPay[i] * 0.5) * overtimeHours[i];
-                grossPay[i] = (hoursWorked[i] * hourlyPay[i]) + overtimePay[i];
-            }//if
-            else if (hoursWorked[i] <= 40){
-                grossPay[i] = hoursWorked[i] * hourlyPay[i];
-            }//else if
-        cout << "Index: " << i << endl;
-        cout << "Hours Worked: " << hoursWorked[i] << endl;
-        }//for
+
 
 /*
-        //Get base Tax Rate based on gross pay
-        if (grossPay[i] > 1000) taxRate[i] = 0.3;
-        else if (grossPay[i] > 800) taxRate[i] = 0.2;
-        else if (grossPay[i] > 500) taxRate[i] = 0.1;
-        else taxRate[i] = 0.0;
 
-        //Modify base tax rate based on marital status:
-        //Married (NC), Head of Household (-5%), Single (+5%)
-        if ((maritalStatus[i] == 's') || (maritalStatus[i] == 'S')) taxRate[i] += 0.05;
-        if ((maritalStatus[i] == 'h') || (maritalStatus[i] == 'H')) taxRate[i] -= 0.05;
-        
-        //Catch negative tax rates and correct to 0
-        if (taxRate[i] < 0) taxRate[i] = 0;
 
-        //Determine Net Pay
-        taxAmount[i] = grossPay[i] * taxRate[i];
-        netPay[i] = grossPay[i] - taxAmount[i];
+
         
 
         
