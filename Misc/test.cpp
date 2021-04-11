2032b1a2c6aec8ed3f083e1baeea11f751adc3e9
wcook499 <w.cook499@gmail.com>
2021-04-11 16:05:52 -0400

    copy files from previous module

--- Misc/test.cpp
+++ Misc/test.cpp
@@ -1,33 +1,16 @@
+#include <iostream>
 #include <fstream>
-#include<iostream>
-#include <string.h>
+#include <iomanip>
+#include <string>
 using namespace std;
 
-class person{
-    private: char name[20];
-    char telephone[18];
-    ifstream fin;
-    public:  person(){ fin.open("data.in"); }
-    ~person(){fin.close(); }
-    search (char []);
-};
+class shape{
+    public: int height,
+                width;
+}
 
-person :: paint(char searchname[]){
-    while(fin>>name>>telephone)
-    if(strcmp(searchname, name) == 0){
-     	cout<<"FOUND!"<<endl;
-        cout<<"NAME : "<<name<<endl<<"PHONE NO : "<<telephone<<endl;
-        return 1;
-    }//IF
-    cout<<"NOT FOUND..."<<endl;
-    return 0;
-}//SEARCH
+class square: public shape{
+   public: int area;
 
-
-void main(){
-    person employee;
-    char nametosearch[20];
-    cout<<"ENTER NAME TO SEARCH : ";
-    cin>>nametosearch;
-    employee.search(nametosearch);
-}//MAIN
+   
+}
