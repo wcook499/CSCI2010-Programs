8dd42efc000346822bbb4d3d9b24ea20d20c4418
wcook499 <w.cook499@gmail.com>
2021-04-21 22:01:58 -0400

    DONE

--- Payroll M6 - Inheritance/Inheritance.cpp
+++ Payroll M6 - Inheritance/Inheritance.cpp
@@ -25,171 +25,195 @@ their net pay (ascending order).
 #include <string>
 using namespace std;
 
-
 class Employee{
-	//data members
-	int 		EmployeeID,
-				NumEmployees;
-	string		FirstName,
-				LastName;
-	char		MaritalStatus;
-	float		HourlyRate,
-				HoursWorked,
-				OvertimeHours,
-				TaxRate,
-				TaxAmount,
-				RegularPay,
-				OvertimePay,
-				GrossPay,
-				NetPay,
-				NetPayAverage,
-				NetPayTotal;
-				
-	ifstream 	fin;
+   
+	public: 
+	int		EmployeeID;
+	float	HoursWorked,
+			BasePay,
+			GrossPay,
+			HourlyRate,
+			NetPay,
+			OvertimeHours,
+			OvertimePay,
+			RegularPay,
+			TaxRate,
+			TaxAmount;
+	string	FirstName,
+			LastName;
+	char	MaritalStatus;
 
-	public:		Employee();			//constructor
-				~Employee();		//destructor
 
-	void 		PrintReport(),
-				PrintDataHeading(),
-				PrintData(),
-				GetOvertimeHours(),
-				GetOvertimePay(),
-				GetRegularPay(),
-				GetGrossPay(),
-				GetTaxRate(),
-				GetTaxAmount(),
-				GetNetPay(),
-				GetNetPayAverage(),
-				PrintNetPayAverage();
+	void SetVariables( string iFirstName, string iLastName, char iMaritalStatus, int iEmployeeID, float iHoursWorked, float iBasePay){
+		
+		FirstName = iFirstName;
+		LastName = iLastName;
+		MaritalStatus = iMaritalStatus;
+		EmployeeID = iEmployeeID;
+		HoursWorked = iHoursWorked;
+		BasePay = iBasePay;
+	}
+
+	virtual float GetGrossPay() = 0;
+
+	float GetTaxAmount(){
+		TaxRate = 0.3;
+		TaxAmount = TaxRate * GrossPay;
+		return TaxAmount;
+	}
+
+	float GetOvertimeHours(){
+		if (HoursWorked > 40){
+            OvertimeHours = HoursWorked - 40;
+        }//if
+        
+		else if (HoursWorked <= 40){
+            OvertimeHours = 0;
+        }//else if
+		
+		return OvertimeHours;
+	}
+
+	float GetNetPay(){
+		NetPay = GrossPay - GetTaxAmount();
+		return NetPay;
+	}
+
+
+
+	void PrintData(){
+		cout << fixed << setprecision(2) << setw(10) << FirstName << setw(10)
+		<< LastName << setw(10) << MaritalStatus << setw(10) << EmployeeID
+		<< setw(10) << HoursWorked << setw(10) << HourlyRate << setw(10)
+		<< OvertimeHours << setw(10) << OvertimePay << setw(10) << RegularPay
+		<< setw(10) << GrossPay << setw(10) << TaxAmount << setw(10) << NetPay
+		<< endl;
+	}
+
+
+
 
 };
 
-Employee::Employee(){	
-    fin.open("employee.in");
-}//constructor
+
+class Salary: public Employee{
+	
+	public: float GetGrossPay(){
+		OvertimeHours = GetOvertimeHours();
+		RegularPay = BasePay / 52;
+		HourlyRate = RegularPay / 40;
+
+		OvertimePay = OvertimeHours * (HourlyRate * 1.5);
+
+		GrossPay = RegularPay + OvertimePay;
+
+		return GrossPay;
+	}//
+
+	Salary(){};
+
+};
 
 
-Employee::~Employee(){	
-    fin.close();
-}//destructor
+class Hourly: public Employee{
+	
+	public: float GetGrossPay(){
+		HourlyRate = BasePay;
+		OvertimeHours = GetOvertimeHours();
+		RegularPay = BasePay * HoursWorked;
+		OvertimePay = OvertimeHours * (BasePay * 1.5);
+		GrossPay = RegularPay + OvertimePay;
 
-class Hourly : public Employee{
+		return GrossPay;
+	}//
 
-}//Hourly
-
-class Salary : public Employee{
-
-}//Salary
-
-void Employee::PrintReport(){
-	NumEmployees = 0;
-	NetPayTotal = 0;
-
-	PrintDataHeading();
-
-	while(fin >> FirstName >> LastName >> MaritalStatus >> EmployeeID 
-		  >> HoursWorked >> HourlyRate){
-		GetOvertimeHours();
-		GetOvertimePay();
-		GetRegularPay();
-		GetGrossPay();
-		GetTaxRate();
-		GetTaxAmount();
-		GetNetPay();
-		PrintData();
-		NumEmployees++;
-		NetPayTotal = NetPayTotal + NetPay;
-	}//while
-
-	GetNetPayAverage();
-	PrintNetPayAverage();
-}//PrintReport
+	Hourly(){};
+};
 
 
-void Employee::PrintDataHeading(){
-	cout << "              ██████╗ ██████╗        ███████╗██████╗ ██████╗  █████╗ ██╗  ██╗██╗███╗   ███╗██╗███████╗" << endl;
-    cout << "              ██╔══██╗██╔══██╗       ██╔════╝██╔══██╗██╔══██╗██╔══██╗██║  ██║██║████╗ ████║██║██╔════╝" << endl;
-    cout << "              ██║  ██║██████╔╝       █████╗  ██████╔╝██████╔╝███████║███████║██║██╔████╔██║██║███████╗" << endl;
-    cout << "              ██║  ██║██╔══██╗       ██╔══╝  ██╔══██╗██╔══██╗██╔══██║██╔══██║██║██║╚██╔╝██║██║╚════██║ PAYROLL" << endl;
-    cout << "              ██████╔╝██║  ██║██╗    ███████╗██████╔╝██║  ██║██║  ██║██║  ██║██║██║ ╚═╝ ██║██║███████║ INSTITUTE" << endl;
-    cout << "              ╚═════╝ ╚═╝  ╚═╝╚═╝    ╚══════╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝     ╚═╝╚═╝╚══════╝" << endl;
-    cout << endl;
-    cout << setw(10) << "FIRST NAME" << setw(10) << "LAST NAME" << setw(10)
-         << "STAT" <<  setw(10) << "SSN" << setw(10) << "HW" << setw(10) << "HR"
-         << setw(10) << "OTH" << setw(10) << "OTP" << setw(10) << "REGP"
-         << setw(10) << "GROSS" << setw(10) << "TAX" << setw(10) << "NET" << endl;
-    cout << string(120, '=') << endl;
-}//PrintHeading
+	void PrintDataHeading(){
+		cout << endl << endl;
+		cout << "              ██████╗ ██████╗        ███████╗██████╗ ██████╗  █████╗ ██╗  ██╗██╗███╗   ███╗██╗███████╗" << endl;
+    	cout << "              ██╔══██╗██╔══██╗       ██╔════╝██╔══██╗██╔══██╗██╔══██╗██║  ██║██║████╗ ████║██║██╔════╝" << endl;
+    	cout << "              ██║  ██║██████╔╝       █████╗  ██████╔╝██████╔╝███████║███████║██║██╔████╔██║██║███████╗" << endl;
+    	cout << "              ██║  ██║██╔══██╗       ██╔══╝  ██╔══██╗██╔══██╗██╔══██║██╔══██║██║██║╚██╔╝██║██║╚════██║ PAYROLL" << endl;
+    	cout << "              ██████╔╝██║  ██║██╗    ███████╗██████╔╝██║  ██║██║  ██║██║  ██║██║██║ ╚═╝ ██║██║███████║ INSTITUTE" << endl;
+    	cout << "              ╚═════╝ ╚═╝  ╚═╝╚═╝    ╚══════╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝     ╚═╝╚═╝╚══════╝" << endl;
+    	cout << endl;
+    	cout << setw(10) << "FIRST NAME" << setw(10) << "LAST NAME" << setw(10)
+        	 << "STAT" <<  setw(10) << "SSN" << setw(10) << "HW" << setw(10) << "HR"
+        	 << setw(10) << "OTH" << setw(10) << "OTP" << setw(10) << "REGP"
+        	 << setw(10) << "GROSS" << setw(10) << "TAX" << setw(10) << "NET" << endl;
+    	cout << string(120, '=') << endl;
+	}
 
 
-void Employee::PrintData(){
-	cout << fixed << setprecision(2) << setw(10) << FirstName << setw(10)
-	<< LastName << setw(10) << MaritalStatus << setw(10) << EmployeeID
-	<< setw(10) << HoursWorked << setw(10) << HourlyRate << setw(10)
-	<< OvertimeHours << setw(10) << OvertimePay << setw(10) << RegularPay
-	<< setw(10) << GrossPay << setw(10) << TaxAmount << setw(10) << NetPay
-	<< endl;
-}//PrintData
+	void PrintMinMaxHeading(){
+		cout << endl << endl;
+		cout << "NET PAY MIN/MAX" << endl;
+		cout << string(120, '=') << endl;
+	}
 
 
-void Employee::GetOvertimeHours(){
-        if (HoursWorked > 40){
-            OvertimeHours = HoursWorked - 40;
-        }//if
-        else if (HoursWorked <= 40){
-            OvertimeHours = 0;
-        }//else if
-}//GetOvertimeHours
+	void PointerSort(Employee **EmployeeList, int n){
+		for(int i=0; i<n-1; i++){
+			for(int j=i+1; j<n; j++){
+				if(EmployeeList[i]->NetPay > EmployeeList[j]->NetPay){
+					Employee *Temp = EmployeeList[i];
+					EmployeeList[i] = EmployeeList[j];
+					EmployeeList[j] = Temp;
+				}
+			}
+		}
+	}
 
-
-void Employee::GetOvertimePay(){
-	OvertimePay = OvertimeHours * (HourlyRate * 1.5);
-}
-
-
-void Employee::GetRegularPay(){
-	RegularPay = HourlyRate * HoursWorked;
-}//GetRegularPay
-
-
-void Employee::GetGrossPay(){
-	GrossPay = RegularPay + OvertimePay;
-}//GetGrossPay
-
-
-void Employee::GetTaxRate(){
-	TaxRate = .3;
-}//GetTaxRate
-
-
-void Employee::GetTaxAmount(){
-	TaxAmount = GrossPay * TaxRate;
-}//GetTaxAmount
-
-
-void Employee::GetNetPay(){
-	NetPay = GrossPay - TaxAmount;
-}//GetNetPay
-
-
-void Employee::GetNetPayAverage(){
-	NetPayAverage = NetPayTotal / NumEmployees;
-}//GetNetPayAverage
-
-
-void Employee::PrintNetPayAverage(){
-	cout << endl << endl;
-    cout << string(120, '-') << endl;
-	cout << "NET PAY AVERAGE: " << NetPayAverage << endl;
-	cout << string(120, '-') << endl;
-	cout << endl << endl;
-	cout << string(120, '=') << endl;
-}//PrintNetPayAverage
 
 
 int main(){
-	Employee employee;
-	employee.PrintReport();
-	return 0;
+	int 	NumberEmployees = 0,
+			ArraySize = 16,
+			EmployeeID;
+	char	SalaryOrHourly;
+	float	BasePay,
+			HoursWorked;
+	string	FirstName,
+			LastName;
+	char	MaritalStatus;
+
+	Employee *EmployeeList[ArraySize];
+
+	ifstream fin("employee.in");
+
+	while (fin >> SalaryOrHourly >> FirstName >> LastName >> MaritalStatus >> EmployeeID >> HoursWorked >> BasePay){
+
+		if(SalaryOrHourly == 'H'){
+			EmployeeList[NumberEmployees] = new Hourly();
+			EmployeeList[NumberEmployees]->SetVariables(FirstName, LastName, MaritalStatus, EmployeeID, HoursWorked, BasePay);
+			EmployeeList[NumberEmployees]->GetGrossPay();
+			EmployeeList[NumberEmployees]->GetNetPay();
+		}
+
+		if(SalaryOrHourly == 'S'){
+			EmployeeList[NumberEmployees] = new Salary();
+			EmployeeList[NumberEmployees]->SetVariables(FirstName, LastName, MaritalStatus, EmployeeID, HoursWorked, BasePay);
+			EmployeeList[NumberEmployees]->GetGrossPay();
+			EmployeeList[NumberEmployees]->GetNetPay();
+		}
+	NumberEmployees++;
+
+	}
+	
+	PrintDataHeading();
+	
+	PointerSort(EmployeeList, NumberEmployees);
+
+	for (int i=0; i<NumberEmployees; i++){
+		EmployeeList[i]->PrintData();
+	}
+
+	PrintMinMaxHeading();
+
+	cout << "Max Net Pay: " << setw(10) << EmployeeList[NumberEmployees-1]->NetPay << endl;
+	cout << "Min Net Pay: " << setw(10) << EmployeeList[0]->NetPay << endl;
+return 0;
 }
