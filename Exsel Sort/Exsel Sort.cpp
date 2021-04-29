229db5f227fe9c32555b14b6fd95ea25f5488368
wcook499 <w.cook499@gmail.com>
2021-04-28 22:55:05 -0400

    exsel sort

---
+++ Exsel Sort/Exsel Sort.cpp
@@ -0,0 +1,231 @@
+/*
+
+WILLIAM COOK
+04/04/21
+Module 18 Assignment
+
+
+Expand the Employee Payroll program to include hourly based and salary based
+employees. This phase uses an array of employee objects, inheritance for
+different classes of employees, and polymorphism for salary computation. The 52
+week yearly salary as well as number of overtime hours worked by a salary based
+employee is given). For salary based employees, to find the regular (gross) pay
+for a week, divide the salary by 52. To compute the overtime pay for a salary
+based employee, first find the hourly rate by dividing the gross pay by 40, and
+then compute overtime pay. For every employee, overtime pay, tax amount, and 
+net pay must also be computed. In addition, the program should find the minimum
+and maximum net pay of all employees as well as sort the employees based on
+their net pay (ascending order).
+
+*/
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+class Employee{
+   
+	public: 
+	int		EmployeeID;
+	float	HoursWorked,
+			BasePay,
+			GrossPay,
+			HourlyRate,
+			NetPay,
+			OvertimeHours,
+			OvertimePay,
+			RegularPay,
+			TaxRate,
+			TaxAmount;
+	string	FirstName,
+			LastName;
+	char	MaritalStatus;
+
+
+	void SetVariables( string iFirstName, string iLastName, char iMaritalStatus, int iEmployeeID, float iHoursWorked, float iBasePay){
+		
+		FirstName = iFirstName;
+		LastName = iLastName;
+		MaritalStatus = iMaritalStatus;
+		EmployeeID = iEmployeeID;
+		HoursWorked = iHoursWorked;
+		BasePay = iBasePay;
+	}
+
+	virtual float GetGrossPay() = 0;
+
+	float GetTaxAmount(){
+		TaxRate = 0.3;
+		TaxAmount = TaxRate * GrossPay;
+		return TaxAmount;
+	}
+
+	float GetOvertimeHours(){
+		if (HoursWorked > 40){
+            OvertimeHours = HoursWorked - 40;
+        }//if
+        
+		else if (HoursWorked <= 40){
+            OvertimeHours = 0;
+        }//else if
+		
+		return OvertimeHours;
+	}
+
+	float GetNetPay(){
+		NetPay = GrossPay - GetTaxAmount();
+		return NetPay;
+	}
+
+
+
+	void PrintData(){
+		cout << fixed << setprecision(2) << setw(10) << FirstName << setw(10)
+		<< LastName << setw(10) << MaritalStatus << setw(10) << EmployeeID
+		<< setw(10) << HoursWorked << setw(10) << HourlyRate << setw(10)
+		<< OvertimeHours << setw(10) << OvertimePay << setw(10) << RegularPay
+		<< setw(10) << GrossPay << setw(10) << TaxAmount << setw(10) << NetPay
+		<< endl;
+	}
+
+
+
+
+};
+
+
+class Salary: public Employee{
+	
+	public: float GetGrossPay(){
+		OvertimeHours = GetOvertimeHours();
+		RegularPay = BasePay / 52;
+		HourlyRate = RegularPay / 40;
+
+		OvertimePay = OvertimeHours * (HourlyRate * 1.5);
+
+		GrossPay = RegularPay + OvertimePay;
+
+		return GrossPay;
+	}//
+
+	Salary(){};
+
+};
+
+
+class Hourly: public Employee{
+	
+	public: float GetGrossPay(){
+		HourlyRate = BasePay;
+		OvertimeHours = GetOvertimeHours();
+		RegularPay = BasePay * HoursWorked;
+		OvertimePay = OvertimeHours * (BasePay * 1.5);
+		GrossPay = RegularPay + OvertimePay;
+
+		return GrossPay;
+	}//
+
+	Hourly(){};
+};
+
+
+	void PrintDataHeading(){
+		cout << endl << endl;
+		cout << "              ██████╗ ██████╗        ███████╗██████╗ ██████╗  █████╗ ██╗  ██╗██╗███╗   ███╗██╗███████╗" << endl;
+    	cout << "              ██╔══██╗██╔══██╗       ██╔════╝██╔══██╗██╔══██╗██╔══██╗██║  ██║██║████╗ ████║██║██╔════╝" << endl;
+    	cout << "              ██║  ██║██████╔╝       █████╗  ██████╔╝██████╔╝███████║███████║██║██╔████╔██║██║███████╗" << endl;
+    	cout << "              ██║  ██║██╔══██╗       ██╔══╝  ██╔══██╗██╔══██╗██╔══██║██╔══██║██║██║╚██╔╝██║██║╚════██║ PAYROLL" << endl;
+    	cout << "              ██████╔╝██║  ██║██╗    ███████╗██████╔╝██║  ██║██║  ██║██║  ██║██║██║ ╚═╝ ██║██║███████║ INSTITUTE" << endl;
+    	cout << "              ╚═════╝ ╚═╝  ╚═╝╚═╝    ╚══════╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝     ╚═╝╚═╝╚══════╝" << endl;
+    	cout << endl;
+    	cout << setw(10) << "FIRST NAME" << setw(10) << "LAST NAME" << setw(10)
+        	 << "STAT" <<  setw(10) << "SSN" << setw(10) << "HW" << setw(10) << "HR"
+        	 << setw(10) << "OTH" << setw(10) << "OTP" << setw(10) << "REGP"
+        	 << setw(10) << "GROSS" << setw(10) << "TAX" << setw(10) << "NET" << endl;
+    	cout << string(120, '=') << endl;
+	}
+
+
+	void PrintMinMaxHeading(){
+		cout << endl << endl;
+		cout << "NET PAY MIN/MAX" << endl;
+		cout << string(120, '=') << endl;
+	}
+
+
+	void swap(Employee **EmployeeList, int i, int j){
+    	Employee *temp=EmployeeList[i];
+    	EmployeeList[i]=EmployeeList[j];
+    	EmployeeList[j]=temp;
+	}
+
+void Sort(Employee **EmployeeList, int n){
+int lower, upper, sortflag, sml, scan;
+    lower = 0;                                      
+    upper = n-1;                                    
+    sortflag = 1;                                  
+    while((lower<upper)&&(sortflag==1)){           
+		sml = lower;                                
+        sortflag = 0;                               
+        while(scan<n-1){                   
+            if(EmployeeList[scan]->NetPay > EmployeeList[scan+1]->NetPay){
+				swap(EmployeeList,scan,scan+1);
+                sortflag=1;
+                if(EmployeeList[scan]->NetPay < EmployeeList[sml]->NetPay) sml=scan;
+            }
+        scan++;
+        }
+    swap(EmployeeList, lower, sml);        
+    }
+}
+
+int main(){
+	int 	NumberEmployees = 0,
+			ArraySize = 16,
+			EmployeeID;
+	char	SalaryOrHourly;
+	float	BasePay,
+			HoursWorked;
+	string	FirstName,
+			LastName;
+	char	MaritalStatus;
+
+	Employee *EmployeeList[ArraySize];
+
+	ifstream fin("employee.in");
+
+	while (fin >> SalaryOrHourly >> FirstName >> LastName >> MaritalStatus >> EmployeeID >> HoursWorked >> BasePay){
+
+		if(SalaryOrHourly == 'H'){
+			EmployeeList[NumberEmployees] = new Hourly();
+			EmployeeList[NumberEmployees]->SetVariables(FirstName, LastName, MaritalStatus, EmployeeID, HoursWorked, BasePay);
+			EmployeeList[NumberEmployees]->GetGrossPay();
+			EmployeeList[NumberEmployees]->GetNetPay();
+		}
+
+		if(SalaryOrHourly == 'S'){
+			EmployeeList[NumberEmployees] = new Salary();
+			EmployeeList[NumberEmployees]->SetVariables(FirstName, LastName, MaritalStatus, EmployeeID, HoursWorked, BasePay);
+			EmployeeList[NumberEmployees]->GetGrossPay();
+			EmployeeList[NumberEmployees]->GetNetPay();
+		}
+	NumberEmployees++;
+
+	}
+	
+	PrintDataHeading();
+	
+	Sort(EmployeeList, NumberEmployees);
+
+	for (int i=0; i<NumberEmployees; i++){
+		EmployeeList[i]->PrintData();
+	}
+
+	PrintMinMaxHeading();
+
+	cout << "Max Net Pay: " << setw(10) << EmployeeList[NumberEmployees-1]->NetPay << endl;
+	cout << "Min Net Pay: " << setw(10) << EmployeeList[0]->NetPay << endl;
+return 0;
+}
--- Misc/test.cpp
+++ Misc/test.cpp
@@ -1,132 +1,39 @@
 #include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <string>
 using namespace std;
 
-class Employee{
-   
-	public: 
-	int		EmployeeID;
-	float	HoursWorked,
-			BasePay,
-			GrossPay,
-			HourlyRate,
-			NetPay,
-			OvertimeHours,
-			RegularPay,
-			TaxRate,
-			TaxAmount;
-	string	FirstName,
-			LastName;
 
-	void SetVariables( int iEmployeeID, string iFirstName, string iLastName, float iBasePay, float iHoursWorked){
-		EmployeeID = iEmployeeID;
-		FirstName = iFirstName;
-		LastName = iLastName;
-		BasePay = iBasePay;
-		HoursWorked = iHoursWorked;
-	}
+int swap(int slot[], int i, int j){
+    int temp;
+    temp=slot[i];
+    slot[i]=slot[j];
+    slot[j]=temp;
+    return 0;
+}
 
-	virtual float GetGrossPay() = 0;
+int main(){-
+    int slot[10]={8,3,1,5,7,10,4,9,6,2};
+    int n=10,i;
+    int lower, upper, sortflag, sml, scan;
+    lower = 0;                                      // lower = 0 (First Index of the array)
+    upper = n-1;                                    // upper = n-1 (The last index of the array)
+    sortflag = 1;                                   // sortflag tells us if the array is sorted yet or not.
+    while((lower<upper)&&(sortflag==1)){            // ?????
+        sml = lower;                                // First, set smallest known value to the first index of the array
+        sortflag = 0;                               // Set the sort flag to zero.  If no swaps are needed, we know the array is sorted
+        scan = lower+1;                             // We start scanning at the first index of the array
+        while(scan<=upper-lower){                   // ??? What does this do? Does this force the while loop to stop half way?
+            if(slot[scan]>slot[scan+1]){            // If the first index value is larger than the second index value...
+                swap(slot,scan,scan+1);             // SWAP EM
+                sortflag=1;                         // A swap was needed, so we know the array needs another pass
+                if(slot[scan]<slot[sml]) sml=scan;  // If the current index value is less than the smallest known value, set the smallest known value to that
+            }
+        scan++;                                     // increment scan to move to the next index
+        }
+    swap(slot, lower, sml);                         // swap the current index with the smallest known value... hmm is this the key to "exsel" sort?... seems to cut needed passes short?
+    }
 
-	float GetTaxAmount(){
-		TaxRate = 0.3;
-		TaxAmount = TaxRate * GrossPay;
-		return TaxAmount;
-	}
-
-	float GetOvertimeHours(){
-		if (HoursWorked > 40){
-            OvertimeHours = HoursWorked - 40;
-        }//if
-        
-		else if (HoursWorked <= 40){
-            OvertimeHours = 0;
-        }//else if
-		
-		return OvertimeHours;
-	}
-
-	float GetNetPay(){
-		NetPay = GrossPay - GetTaxAmount();
-		return NetPay;
-	}
-
-	void Print(){
-		cout << EmployeeID << " " << FirstName << " " << LastName << NetPay << endl;
-	}
-};
-
-
-class Salary: public Employee{
-	
-	public: float GetGrossPay(){
-		OvertimeHours = GetOvertimeHours();
-		RegularPay = BasePay / 52;
-		HourlyRate = RegularPay / 40;
-
-		GrossPay = RegularPay + (OvertimeHours * (HourlyRate * 1.5));
-
-		return GrossPay;
-	}//
-
-	Salary(){};
-
-};
-
-
-class Hourly: public Employee{
-	public:
-	float	HourlyRate;
-	int		EmployeeID;
-
-	public: float GetGrossPay(){
-		OvertimeHours = GetOvertimeHours();
-		RegularPay = BasePay * HoursWorked;
-		GrossPay = RegularPay + (OvertimeHours * (HourlyRate * 1.5));
-
-		return GrossPay;
-	}//
-
-	Hourly(){};
-};
-
-
-
-int main(){
-	int 	i = 0,
-			ArraySize = 16,
-			EmployeeID;
-	char	SalaryOrHourly;
-	float	BasePay,
-			HoursWorked;
-	string	FirstName,
-			LastName;
-
-	Employee *EmployeeList[ArraySize];
-
-	ifstream fin("employee.in");
-
-	while (fin >> SalaryOrHourly >> EmployeeID >> FirstName >> LastName >> BasePay >> HoursWorked){
-
-		if(SalaryOrHourly == 'H'){
-			EmployeeList[i] = new Hourly();
-			EmployeeList[i]->SetVariables(EmployeeID, FirstName, LastName, BasePay, HoursWorked);
-			EmployeeList[i]->GetGrossPay();
-			EmployeeList[i]->GetNetPay();
-			EmployeeList[i]->Print();
-		}
-
-		if(SalaryOrHourly == 'S'){
-			EmployeeList[i] = new Salary();
-			EmployeeList[i]->SetVariables(EmployeeID, FirstName, LastName, BasePay, HoursWorked);
-			EmployeeList[i]->GetGrossPay();
-			EmployeeList[i]->GetNetPay();
-			EmployeeList[i]->Print();
-		}
-	i++;
-	}
-	
+cout << "after sort: " << endl;
+for(i=1;i<n+1;i++) cout << slot[i]<<" ";
+cout << endl;
 return 0;
 }
